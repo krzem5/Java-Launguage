2db3be1180eaf22a469bfb50f01f945f9d7eb582
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:53:13 +0100

    Push Update

---
+++ com/krzem/CJL/modules/builtin.java
@@ -0,0 +1,15 @@
+import java.lang.Object;
+
+
+
+public class builtin extends Object{
+	public void print(Object o){
+		System.out.print(o);
+	}
+
+
+
+	public void println(Object o){
+		System.out.println(o);
+	}
+}

ef7c76e4c85d8174a16fc4e0d494514f1a92d73f
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 15:52:05 +0100

    Push Update

---
+++ com/krzem/CJL/C/ClassList.java
@@ -0,0 +1,55 @@
+package com.krzem.CJL.C;
+
+
+
+import java.util.ArrayList;
+
+
+
+public class ClassList{
+	private ArrayList<Class> Cl;
+
+
+
+	public ClassList(){
+		this.Cl=new ArrayList<Class>();
+	}
+
+
+
+	public void add(Class c){
+		this.Cl.add(c);
+	}
+
+
+
+	public ClassList clone(){
+		ClassList cl=new ClassList();
+		for (Class c:this.Cl){
+			cl.add(c);
+		}
+		return cl;
+	}
+
+
+
+	public boolean has(String nm){
+		for (Class c:this.Cl){
+			if (c.name.equals(nm)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+
+	public Class find_name(String nm){
+		for (Class c:this.Cl){
+			if (c.name.equals(nm)){
+				return c;
+			}
+		}
+		return null;
+	}
+}

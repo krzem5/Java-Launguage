d666adcff5398f494ce2232ae03e0c15fac9ca4a
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:38:55 +0200

    Push Update 04/10/2021, 09:38:22

--- build.bat
+++ build.bat
@@ -1,6 +1,3 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/CJL/CJL.java
-java com/krzem/CJL/CJL -c prog.cjl -d -r prog
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
